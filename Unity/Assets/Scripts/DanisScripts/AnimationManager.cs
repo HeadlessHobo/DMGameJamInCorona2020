083faf8970865954f9a56b2517931cf8b3ff0854
Dani Felix M Laynes <dani@videndjurs.dk>
2020-09-26 14:23:26 +0200

    ExplosionAndAnimation

---
+++ Unity/Assets/Scripts/DanisScripts/AnimationManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+
+    public Animator ExplosionAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //DEBUGS
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            ExplosionAni();
+        }
+    }
+
+    public void ExplosionAni()
+    {
+        ExplosionAnimator.Play("Explosion");
+    }
+
+}
